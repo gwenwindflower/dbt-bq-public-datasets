d409b37ed2e8aa26bd01aff8c7a76dbdfa430815
gwen windflower <gwenwindflower@gmail.com>
2023-02-06 17:48:13 -0600

    Add initial staging model for subway

---
+++ dbt_nyc/models/staging/subway/stg_subway__routes.sql
@@ -0,0 +1,22 @@
+with
+
+source as (
+    
+    select * from {{ source('subway', 'routes') }}
+
+),
+
+renamed as (
+
+    select 
+        route_id,
+        route_short_name,
+        route_long_name,
+        route_describe as description,
+        route_type
+
+    from source
+
+)
+
+select * from renamed
