19b8b718377818e0b043a738db8bb12a894c9681
gwen windflower <gwenwindflower@gmail.com>
2023-02-07 13:09:41 -0600

    Add another citibike model

---
+++ dbt_nyc/models/staging/citibike/stg_citibike__trips.sql
@@ -0,0 +1,43 @@
+with 
+
+source as (
+
+    select * from {{ source('citibike', 'citibike_trips') }}
+
+),
+
+renamed as (
+
+    select
+        -- ids
+        {{ dbt_utils.generate_surrogate_key(['bikeid', 'starttime', 'stoptime']) }} as trip_id,
+        bikeid as bike_id,
+        start_station_id,
+        end_station_id,
+        
+        -- numeric
+        tripduration as trip_duration,
+        birth_year,
+
+        -- text
+        start_station_name,
+        end_station_name,
+        usertype as user_type,
+        gender,
+        customer_plan,
+        
+        -- geom
+        start_station_latitude,
+        start_station_longitude,
+        end_station_latitude,
+        end_station_longitude,
+
+        -- datetimes
+        starttime as start_time,
+        stoptime as stop_time
+
+    from source
+
+)
+
+select * from renamed
